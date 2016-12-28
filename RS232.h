7fa746311db88cee887b6fdf195cbdd15f29d04c
Dmitry7777 <list-dmitrij@mail.ru>
2016-12-28 20:31:27 +0300

    Update RS232.h

--- RS232.h
+++ RS232.h
@@ -1,1 +1,55 @@
+//
+#define _RS232_H
+#ifndef _RS232_H
 
+//
+#include <Arduino.h >
+#include <RS232.h>
+
+//
+#define RS232_DCD
+#define RS232_RXD
+#define RS232_TXD
+#define RS232_DTR
+#define RS232_DSR
+#define RS232_RTS
+#define RS232_CTS
+#define RS232_RI
+
+//
+#endif
+
+class RS232{
+
+public:
+//
+void available(); //
+void availableForWrite(); //
+void begin(); //
+void end(); //
+void find(); //
+void findUntil(); //
+void flush
+void parse Float(); //
+void parseInt(); //
+void peek(); //
+void print(); //
+void println(); //
+void read(); //
+void readBytes(); //
+void readBytesUntil(); //
+void readString(); //
+void readStringUntil(); //
+void setTimeout(); //
+void write(); //
+
+
+private:
+//
+
+protected:
+//
+
+};
+
+extern RS232_class;
